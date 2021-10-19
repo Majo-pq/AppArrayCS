b65fcc1cc9af56cb414c09b1ec3d244481361051
Maria Jose Pecina Quintana <h21050059@monclova.tecnm.mx>
2021-10-19 14:34:15 -0500

    Agregar archivos de proyecto.

---
+++ Celular.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppArrayCS
+{
+    class Celular
+    {
+        private string marca;
+        private string color;
+        private string capacidadAlmacenamiento;
+
+        public string Marca
+        { 
+            get
+            {
+                return marca;
+            }
+            set
+            {
+                marca = value;
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+        public string CapacidadAlmacenamiento
+        {
+            get
+            {
+                return capacidadAlmacenamiento;
+            }
+            set
+            {
+                capacidadAlmacenamiento = value;
+            }
+        }
+        public Celular()
+        {
+            marca = "";
+            color = "";
+            capacidadAlmacenamiento = "";
+        }
+        public Celular(string marca, string color, string capacidadAlmacenamiento)
+        {
+            this.marca = marca;
+            this.color = color;
+            this.capacidadAlmacenamiento = capacidadAlmacenamiento;
+        }
+        public override string ToString()
+        {
+            return marca + " - " + color + " - " + capacidadAlmacenamiento;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppArrayCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Celular[] a = new Celular[5];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = new Celular();
+                Console.WriteLine("Marca del celular");
+                a[i].Marca = "";
+                a[i].Marca = Console.ReadLine();
+                Console.WriteLine("Color del celular");
+                a[i].Color = "";
+                a[i].Color = Console.ReadLine();
+                Console.WriteLine("Capacidad de Almacenamiento");
+                a[i].CapacidadAlmacenamiento = "";
+                a[i].CapacidadAlmacenamiento = Console.ReadLine();
+                
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i].ToString());
+            }   
+            Console.ReadKey();
+
+        
+        }
+    }
+}
